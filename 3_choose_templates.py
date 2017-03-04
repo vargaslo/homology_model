154f021b1ef1b0cf55fdadae9744214cec488e80
Ernesto Vargas <vargaslo@gmail.com>
2017-03-04 03:54:46 -0600

    add comments for readability

--- 3_choose_templates.py
+++ 3_choose_templates.py
@@ -7,13 +7,17 @@ def retrieve_best_matches(infile, max_eval):
       with open(outfile, 'w') as fout:
         for line in fin:
 
+            # copy comment lines directly
             if line[0]=='#':
                 fout.write(line)
 
+            # parse non-comment lines
             if line[0]!='#':
                 line_chunks = line.split()
                 idx, code, type, _,_,_,_,first,last,_,seqid,eval = line_chunks[:12]
                 eval = float(eval)
+                
+                # copy line if e-value is below threshold
                 if type=='X' and eval<max_eval:
                     fout.write(line)
     return
